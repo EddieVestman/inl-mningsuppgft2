b6741d7a5c8d04ee66276caab4f1a9261cc43cb2
EddieVestman <edve02001@utb.vaxjo.se>
2019-09-20 09:40:09 +0200

    skrivit kod

---
+++ inlämningsuppgift2/inlämningsuppgift2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inlämningsuppgift2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double radie = 2.4;
+            Console.WriteLine("Cirkelns omkrets:" + CirkelOmkrets(radie));
+            Console.WriteLine("Cirkelns area:" + CirkelArea(radie));
+        }
+        static double CirkelOmkrets(double radie)
+        {
+            return radie * 2 * 3.14;
+        }
+        static double CirkelArea(double radie)
+        {
+            return radie * radie * 3.14;
+        }
+    }
+}
