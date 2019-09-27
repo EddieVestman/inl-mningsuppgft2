6ae3a7a386a0a60753099a637dc463c963604a5b
EddieVestman <edve02001@utb.vaxjo.se>
2019-09-27 09:38:53 +0200

    skrev in koden :DDDDD

---
+++ Metoderuppgfit3/Metoderuppgfit3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Metoderuppgfit3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10, b = 20;
+
+            BytPlats(ref a, ref b);
+
+            Console.WriteLine("a = " + a +" "+ "b = " + b);
+
+
+
+        }
+        public static void BytPlats(ref int a, ref int b)
+        {
+            
+            int temp = a;
+            a = b;
+            b = temp;
+           
+        }
+          
+    }
+}
